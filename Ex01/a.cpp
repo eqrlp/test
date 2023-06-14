7599b8ae2b96f3377f12061826914f9ea2a7a519
taku <taku@wlan-napt-003.u-aizu.ac.jp>
2023-06-14 13:50:05 +0900

    Dice game: initial revision

---
+++ Ex01/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <random>
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define lli long long int
+#define lld long long double
+using namespace std;
+
+random_device rd;
+mt19937 gen(rd());
+ 
+int random(int low, int high)
+{
+    uniform_int_distribution<> dist(low, high);
+    return dist(gen);
+}
+
+
+int main() { 
+    int a,b;
+    a = random(1,6);
+    b = random(1,6);
+    cout << "Rolling dice..." << endl;
+    cout << "Die 1: " << a << endl;
+    cout << "Die 2: " << b << endl;
+    cout << "Total value: " << a+b << endl;
+
+}
